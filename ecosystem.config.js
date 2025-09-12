0779d08da729217d9b6b02e9020cdf0b3626adaa
prokopenko.a <kap.moral22@gmail.com>
2025-09-12 19:38:12 +0300

    eco system for deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'netr-site',
+      script: 'npx',
+      args: 'serve -s dist -l 3012',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3012
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
+};
