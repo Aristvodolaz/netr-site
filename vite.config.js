684e61b4f2ae2cd767b6803bbfd3f73ce86557e4
prokopenko.a <kap.moral22@gmail.com>
2025-09-12 22:55:18 +0300

    new change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    port: 3012,
+    host: true, // Allow external connections
     proxy: {
       '/api': {
         target: 'http://10.171.12.36:3005',
